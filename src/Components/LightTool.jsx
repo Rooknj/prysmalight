abe7733bb200db567ebe36a5452160103c6aec3b
Nick Rook <nick_rook@intuit.com>
2018-04-07 11:58:33 -0700

    Deleted old Light component and cleaned up LightTool

--- src/Components/LightTool.jsx
+++ src/Components/LightTool.jsx
@@ -2,18 +2,12 @@ import React from "react";
 import LightList from "./LightList";
 import AddLight from "./AddLight";
 
-class LightTool extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                <LightList />
-            </div>
-        );
-    }
-}
+const LightTool = props => {
+    return (
+        <div>
+            <LightList />
+        </div>
+    );
+};
 
 export default LightTool;

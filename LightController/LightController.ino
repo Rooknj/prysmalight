0015bff52fab9beb94ab7088cdc9bbcfef8aa2cf
Nick Rook <rooknj@outlook.com>
2018-04-17 21:58:16 -0700

    Added WifiManager and OTA Updates

--- LightController/LightController.ino
+++ LightController/LightController.ino
@@ -9,14 +9,65 @@
          |___/                                           
 */
 
+#include <ESP8266WiFi.h>          // ESP8266 Core WiFi Library
+#include <ESP8266mDNS.h>
+#include <DNSServer.h>            // Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     // Local WebServer used to serve the configuration portal
+#include <ArduinoOTA.h>
+#include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
- 
+#include <PubSubClient.h>
+
 void setup() {
-  // put your setup code here, to run once:
+  // Set Serial Communication rate
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 
+  // Autoconnect to Wifi
+  WiFiManager wifiManager;
+  if (!wifiManager.autoConnect("Nick's Lightapp-ESP8266", "991f76a6ab")) {
+    // (AP-Name, Password)
+    Serial.println("Failed to connect, try resetting the module");
+    delay(3000);
+    ESP.reset();
+    delay(5000);
+  }
+  Serial.println("Connected to Wifi :)");
+
+  // OTA Setup
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start OTA");
+    digitalWrite(LED_BUILTIN, LOW);
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("End OTA");
+    digitalWrite(LED_BUILTIN, HIGH);
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("OTA Ready");
+ 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  ArduinoOTA.handle();
+  //digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on by making the voltage LOW
+  //delay(1000);                      // Wait for a second
+  //digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  //delay(2000);                      // Wait for two seconds
 }

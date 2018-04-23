b4bf3e586aef52e5acd37dfd1899317f73a57aef
Nick Rook <rooknj@outlook.com>
2018-04-22 22:05:59 -0700

    Added Home Assistant Support

--- LightController/LightController.ino
+++ LightController/LightController.ino
@@ -16,25 +16,175 @@
 #include <ArduinoOTA.h>
 #include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include <PubSubClient.h>
-#include <ArduinoJson.h>
+
+#define MQTT_VERSION MQTT_VERSION_3_1_1
+
+
+
+/************ MQTT Setup Variables ******************/
+// MQTT: ID, server IP, port, username and password
+const PROGMEM char* MQTT_CLIENT_ID = "office_rgb_light";
+const PROGMEM char* MQTT_SERVER_IP = "broker.hivemq.com";
+const PROGMEM uint16_t MQTT_SERVER_PORT = 1883;
+//const PROGMEM char* MQTT_USER = "[Redacted]";
+//const PROGMEM char* MQTT_PASSWORD = "[Redacted]";
+
+// MQTT: topics
+// state
+const PROGMEM char* MQTT_LIGHT_STATE_TOPIC = "office/rgb1/light/status";
+const PROGMEM char* MQTT_LIGHT_COMMAND_TOPIC = "office/rgb1/light/switch";
+
+// brightness
+const PROGMEM char* MQTT_LIGHT_BRIGHTNESS_STATE_TOPIC = "office/rgb1/brightness/status";
+const PROGMEM char* MQTT_LIGHT_BRIGHTNESS_COMMAND_TOPIC = "office/rgb1/brightness/set";
+
+// colors (rgb)
+const PROGMEM char* MQTT_LIGHT_RGB_STATE_TOPIC = "office/rgb1/rgb/status";
+const PROGMEM char* MQTT_LIGHT_RGB_COMMAND_TOPIC = "office/rgb1/rgb/set";
+
+// payloads by default (on/off)
+const PROGMEM char* LIGHT_ON = "ON";
+const PROGMEM char* LIGHT_OFF = "OFF";
+
+// buffer used to send/receive data with MQTT
+const uint8_t MSG_BUFFER_SIZE = 20;
+char m_msg_buffer[MSG_BUFFER_SIZE]; 
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
 
 
 
 /************ Data Global Variables ******************/
-bool power = false;
-int brightness = 0;
-int r = 0;
-int g = 0;
-int b = 0;
+// variables used to store the state, the brightness and the color of the light
+boolean m_rgb_state = false;
+uint8_t m_rgb_brightness = 100;
+uint8_t m_rgb_red = 255;
+uint8_t m_rgb_green = 255;
+uint8_t m_rgb_blue = 255;
 
-/************ MQTT Setup Variables ******************/
-const char* mqtt_server = "broker.hivemq.com";
-WiFiClient espClient;
-PubSubClient client(espClient);
-const char* inTopic = "CHANGE_LIGHT_TOPIC";
-const char* outTopic = "LIGHT_CHANGED_TOPIC";
-const char* getStateTopic = "GET_LIGHT_STATE_TOPIC";
-const char* sendStateTopic = "LIGHT_STATE_TOPIC";
+// pins used for the rgb led (PWM)
+const PROGMEM uint8_t RGB_LIGHT_RED_PIN = 5;
+const PROGMEM uint8_t RGB_LIGHT_GREEN_PIN = 0;
+const PROGMEM uint8_t RGB_LIGHT_BLUE_PIN = 4;
+
+
+
+/************ Functions ******************/
+// function called to adapt the brightness and the color of the led
+void setColor(uint8_t p_red, uint8_t p_green, uint8_t p_blue) {
+  // convert the brightness in % (0-100%) into a digital value (0-255)
+  uint8_t brightness = map(m_rgb_brightness, 0, 100, 0, 255);
+
+  analogWrite(RGB_LIGHT_RED_PIN, map(p_red, 0, 255, 0, brightness));
+  analogWrite(RGB_LIGHT_GREEN_PIN, map(p_green, 0, 255, 0, brightness));
+  analogWrite(RGB_LIGHT_BLUE_PIN, map(p_blue, 0, 255, 0, brightness));
+}
+
+// function called to publish the state of the led (on/off)
+void publishRGBState() {
+  if (m_rgb_state) {
+    client.publish(MQTT_LIGHT_STATE_TOPIC, LIGHT_ON, true);
+  } else {
+    client.publish(MQTT_LIGHT_STATE_TOPIC, LIGHT_OFF, true);
+  }
+}
+
+// function called to publish the brightness of the led (0-100)
+void publishRGBBrightness() {
+  snprintf(m_msg_buffer, MSG_BUFFER_SIZE, "%d", m_rgb_brightness);
+  client.publish(MQTT_LIGHT_BRIGHTNESS_STATE_TOPIC, m_msg_buffer, true);
+}
+
+// function called to publish the colors of the led (xx(x),xx(x),xx(x))
+void publishRGBColor() {
+  snprintf(m_msg_buffer, MSG_BUFFER_SIZE, "%d,%d,%d", m_rgb_red, m_rgb_green, m_rgb_blue);
+  client.publish(MQTT_LIGHT_RGB_STATE_TOPIC, m_msg_buffer, true);
+}
+
+// function called when a MQTT message arrived
+void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
+  // concat the payload into a string
+  String payload;
+  for (uint8_t i = 0; i < p_length; i++) {
+    payload.concat((char)p_payload[i]);
+  }
+  // handle message topic
+  if (String(MQTT_LIGHT_COMMAND_TOPIC).equals(p_topic)) {
+    // test if the payload is equal to "ON" or "OFF"
+    if (payload.equals(String(LIGHT_ON))) {
+      if (m_rgb_state != true) {
+        m_rgb_state = true;
+        setColor(m_rgb_red, m_rgb_green, m_rgb_blue);
+        publishRGBState();
+      }
+    } else if (payload.equals(String(LIGHT_OFF))) {
+      if (m_rgb_state != false) {
+        m_rgb_state = false;
+        setColor(0, 0, 0);
+        publishRGBState();
+      }
+    }
+  } else if (String(MQTT_LIGHT_BRIGHTNESS_COMMAND_TOPIC).equals(p_topic)) {
+    uint8_t brightness = payload.toInt();
+    if (brightness < 0 || brightness > 100) {
+      // do nothing...
+      return;
+    } else {
+      m_rgb_brightness = brightness;
+      setColor(m_rgb_red, m_rgb_green, m_rgb_blue);
+      publishRGBBrightness();
+    }
+  } else if (String(MQTT_LIGHT_RGB_COMMAND_TOPIC).equals(p_topic)) {
+    // get the position of the first and second commas
+    uint8_t firstIndex = payload.indexOf(',');
+    uint8_t lastIndex = payload.lastIndexOf(',');
+    
+    uint8_t rgb_red = payload.substring(0, firstIndex).toInt();
+    if (rgb_red < 0 || rgb_red > 255) {
+      return;
+    } else {
+      m_rgb_red = rgb_red;
+    }
+    
+    uint8_t rgb_green = payload.substring(firstIndex + 1, lastIndex).toInt();
+    if (rgb_green < 0 || rgb_green > 255) {
+      return;
+    } else {
+      m_rgb_green = rgb_green;
+    }
+    
+    uint8_t rgb_blue = payload.substring(lastIndex + 1).toInt();
+    if (rgb_blue < 0 || rgb_blue > 255) {
+      return;
+    } else {
+      m_rgb_blue = rgb_blue;
+    }
+    
+    setColor(m_rgb_red, m_rgb_green, m_rgb_blue);
+    publishRGBColor();
+  }
+}
+
+// MQTT connect/reconnect function
+boolean reconnect() {
+  if (client.connect(MQTT_CLIENT_ID)) {
+    Serial.println("INFO: connected");
+    
+    // Once connected, publish an announcement...
+    // publish the initial values
+    publishRGBState();
+    publishRGBBrightness();
+    publishRGBColor();
+
+    // ... and resubscribe
+    client.subscribe(MQTT_LIGHT_COMMAND_TOPIC);
+    client.subscribe(MQTT_LIGHT_BRIGHTNESS_COMMAND_TOPIC);
+    client.subscribe(MQTT_LIGHT_RGB_COMMAND_TOPIC);
+  }
+  return client.connected();
+}
+
 
 
 /************ WIFI Setup ******************/
@@ -43,12 +193,13 @@ void setupWifi() {
   WiFiManager wifiManager;
   if (!wifiManager.autoConnect("Nick's Lightapp-ESP8266", "991f76a6ab")) {
     // (AP-Name, Password)
-    Serial.println("Failed to connect, try resetting the module");
+    Serial.println("ERROR: failed to connect to Wifi");
+    Serial.println("DEBUG: try resetting the module");
     delay(3000);
     ESP.reset();
     delay(5000);
   }
-  Serial.println("Connected to Wifi :)");
+  Serial.println("INFO: connected to Wifi :)");
 }
 
 
@@ -56,30 +207,30 @@ void setupWifi() {
 void setupOTA() {
     // OTA Setup
   ArduinoOTA.onStart([]() {
-    Serial.println("Start OTA");
+    Serial.println("INFO: starting OTA upload");
     digitalWrite(LED_BUILTIN, LOW);
   });
 
   ArduinoOTA.onEnd([]() {
-    Serial.println("End OTA");
+    Serial.println("INFO: OTA upload successful");
     digitalWrite(LED_BUILTIN, HIGH);
   });
 
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
+    Serial.printf("INFO: upload %u%% complete\n", (progress / (total / 100)));
   });
 
   ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    Serial.printf("ERROR: OTA %u: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("OTA Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("OTA Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("OTA Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("OTA Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("OTA End Failed");
   });
 
   ArduinoOTA.begin();
-  Serial.println("OTA Ready");
+  Serial.println("INFO: OTA ready");
 }
 
 
@@ -88,160 +239,33 @@ void setupOTA() {
 void setup() {
   // Set Serial Communication rate
   Serial.begin(115200);
+
+  // init the RGB led
+  pinMode(RGB_LIGHT_BLUE_PIN, OUTPUT);
+  pinMode(RGB_LIGHT_RED_PIN, OUTPUT);
+  pinMode(RGB_LIGHT_GREEN_PIN, OUTPUT);
+  analogWriteRange(255);
+  setColor(0, 0, 0);
+
+  // init the builtin led
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   
-  // Setup Wifi
+  // init the Wifi connection
   setupWifi();
 
-  // Setup OTA
+  // init OTA firmware uploads
   setupOTA();
 
-  // Setup MQTT
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(handleMessage);
-}
-
-
-/************ Publish change in state ******************/
-void publishChange(JsonObject& data) {
-  // Set up JSON response objects
-  StaticJsonBuffer<128> jsonOutBuffer;
-  JsonObject& responsePayload = jsonOutBuffer.createObject();
-  
-  if (data.containsKey("power")) {
-    power = data["power"];
-    //Serial.print("Setting power to: ");
-    //Serial.println(power);
-    digitalWrite(LED_BUILTIN, power ? LOW : HIGH);
-    responsePayload["power"] = power;
-  }
-
-  if (data.containsKey("brightness")) {
-    brightness = data["brightness"];
-    //Serial.print("Setting brightness to: ");
-    //Serial.println(brightness);
-    responsePayload["brightness"] = brightness;
-  }
-
-  if (data.containsKey("color")) {
-    r = data["color"]["r"];
-    g = data["color"]["g"];
-    b = data["color"]["b"];
-//    Serial.print("Setting color to: ");
-//    Serial.print("r = ");
-//    Serial.print(r);
-//    Serial.print(", g = ");
-//    Serial.print(g);
-//    Serial.print(", b = ");
-//    Serial.println(b);
-    JsonObject& color = responsePayload.createNestedObject("color");
-    color["r"] = r;
-    color["g"] = g;
-    color["b"] = b;
-    responsePayload["color"] = color;
-  }
-  
-//  Serial.print("payload Size: ");
-//  Serial.println(responsePayload.size());
-  Serial.println("payload: ");
-  responsePayload.prettyPrintTo(Serial);
-  Serial.println();
-  
-  if(responsePayload.size() > 0) {
-    char responseChar[100];
-    responsePayload.printTo((char*)responseChar, responsePayload.measureLength() + 1);
-    //Serial.println(responseChar);
-    client.publish(outTopic, responseChar);
-  }
-}
-
-
-
-/************ Publish Current State ******************/
-void publishState(JsonObject& data) {
-  if (!data.containsKey("sendState") || !data["sendState"]) {
-    Serial.println("No Send State");
-    return;
-  }
-  
-  // Set up JSON response objects
-  StaticJsonBuffer<128> jsonOutBuffer;
-  JsonObject& responsePayload = jsonOutBuffer.createObject();
-  JsonObject& color = responsePayload.createNestedObject("color");
-
-  // Assign data to JSON response objects
-  responsePayload["power"] = power;
-  responsePayload["brightness"] = brightness;
-  color["r"] = r;
-  color["g"] = g;
-  color["b"] = b;
-  responsePayload["color"] = color;
-  Serial.println("payload: ");
-  responsePayload.prettyPrintTo(Serial);
-  Serial.println();
-
-  // Send status
-  char responseChar[100];
-  responsePayload.printTo((char*)responseChar, responsePayload.measureLength() + 1);
-  //Serial.println(responseChar);
-  client.publish(sendStateTopic, responseChar);
-  
-}
-
-
-
-/************ MQTT onMessage Callback Setup ******************/
-void handleMessage(char* topic, byte* payload, unsigned int length) {
-  
-  // decode the JSON payload
-  StaticJsonBuffer<128> jsonInBuffer;
-  JsonObject& data = jsonInBuffer.parseObject(payload);
-  
-  // Test if parsing succeeds.
-  if (!data.success()) {
-    Serial.println("ERROR: Unable to parse message");
-    return;
-  }
-  // Print the received value to serial monitor for debugging
-  /*
-  Serial.print("Message of length ");
-  Serial.print(length);
-  Serial.print(" arrived on topic: [");
-  Serial.print(topic);
-  Serial.println("] ");
-  data.prettyPrintTo(Serial);
-  Serial.println("\n");
-  */
-
-  if(strcmp(topic, inTopic) == 0){
-    publishChange(data);
-  } else if(strcmp(topic, getStateTopic) == 0) {
-    publishState(data);
-  }
-
-
-}
-
-
-
-/************ MQTT connect/reconnect function ******************/
-long lastReconnectAttempt = 0;
-boolean reconnect() {
-  if (client.connect("arduinoClient")) {
-    Serial.println("Connected");
-    // Once connected, publish an announcement...
-    //client.publish("outTopic","hello world");
-    // ... and resubscribe
-    client.subscribe(inTopic); // For changes
-    client.subscribe(getStateTopic); // For status updates
-  }
-  return client.connected();
+  // init the MQTT connection
+  client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+  client.setCallback(callback);
 }
 
 
 
 /************ Main Loop ******************/
+long lastReconnectAttempt = 0;
 void loop() {
   // Handle OTA requests
   ArduinoOTA.handle();
@@ -250,11 +274,15 @@ void loop() {
   if (!client.connected()) {
     long now = millis();
     if (now - lastReconnectAttempt > 5000) {
-      Serial.println("Attempting to Connect to MQTT Broker");
+      Serial.println("INFO: attempting MQTT connection...");
       lastReconnectAttempt = now;
       // Attempt to reconnect
       if (reconnect()) {
         lastReconnectAttempt = 0;
+      } else {
+        Serial.print("ERROR: failed MQTT Connection, rc=");
+        Serial.println(client.state());
+        Serial.println("DEBUG: try again in 5 seconds");
       }
     }
   } else {

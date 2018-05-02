e33939bc40c896e088d63c4a20dec1af0d41a9df
Nick Rook <nick_rook@intuit.com>
2018-05-02 10:21:05 -0700

    Improved publish script

--- publishServer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo Building docker image
-docker-compose -f docker-compose.prod.yml build
-
-echo Tagging docker image
-docker tag lightapp2-server-prod rooknj/lightapp2:server-latest
-
-echo Pushing docker image
-docker push rooknj/lightapp2:server-latest

03d9dbb9bcf07ea01f8f99a19b1b466b7a334328
Nick Rook <nick_rook@intuit.com>
2018-05-03 15:13:43 -0700

    Added mocks back

--- src/data/schema.js
+++ src/data/schema.js
@@ -1,7 +1,6 @@
 import { makeExecutableSchema, addMockFunctionsToSchema } from "graphql-tools";
 import { importSchema } from "graphql-import";
 import resolvers from "./resolvers";
-//import mocks from "./mocks";
 
 const typeDefs = importSchema(`${__dirname}/typeDefs.graphql`);
 
@@ -9,7 +8,8 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 if (process.env.MOCK) {
   console.log("adding mocks");
-  //addMockFunctionsToSchema({ schema, mocks });
+  const mocks = require("./mocks");
+  addMockFunctionsToSchema({ schema, mocks });
 } else {
   console.log("Not adding mocks");
 }

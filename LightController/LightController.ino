990a5e85988afba121fd1f4b7f7b3ebe87b10935
Nick Rook <rooknj@outlook.com>
2018-05-08 18:42:45 -0700

    Implemented color fade

--- LightController/LightController.ino
+++ LightController/LightController.ino
@@ -85,7 +85,7 @@ byte realBlue = 0;
 // Globals for fade/transitions
 bool startFade = false;
 unsigned long lastLoop = 0;
-int transitionTime = 0;
+int transitionTime = 0; // The amount of time the transition takes in seconds
 bool inFade = false;
 int loopCount = 0;
 int stepR, stepG, stepB;
@@ -152,11 +152,11 @@ bool processJson(char* message) {
   if (root.containsKey("state")) {
     if (strcmp(root["state"], LIGHT_ON) == 0) {
       stateOn = true;
-      setColor(red, green, blue);
+      //setColor(red, green, blue);
     }
     else if (strcmp(root["state"], LIGHT_OFF) == 0) {
       stateOn = false;
-      setColor(0, 0, 0);
+      //setColor(0, 0, 0);
     }
   }
 
@@ -167,7 +167,7 @@ bool processJson(char* message) {
     red = root["color"]["r"];
     green = root["color"]["g"];
     blue = root["color"]["b"];
-    setColor(red, green, blue);
+    //setColor(red, green, blue);
   }
 
   if (root.containsKey("brightness")) {
@@ -180,7 +180,7 @@ bool processJson(char* message) {
     transitionTime = root["transition"];
   }
   else {
-    transitionTime = 0;
+    //transitionTime = 0;
   }
   
   return true;
@@ -335,6 +335,55 @@ void setup() {
 
 
 
+/************ Crossfade transition function ******************/
+void handleCrossfade() {
+  if (startFade) {
+    // If we don't want to fade, skip it.
+    if (transitionTime == 0) {
+      setColor(realRed, realGreen, realBlue);
+
+      redVal = realRed;
+      grnVal = realGreen;
+      bluVal = realBlue;
+
+      startFade = false;
+    }
+    else {
+      loopCount = 0;
+      stepR = calculateStep(redVal, realRed);
+      stepG = calculateStep(grnVal, realGreen);
+      stepB = calculateStep(bluVal, realBlue);
+
+      inFade = true;
+    }
+  }
+
+  if (inFade) {
+    startFade = false;
+    unsigned long now = millis();
+    if (now - lastLoop > transitionTime) {
+      if (loopCount <= 1020) {
+        lastLoop = now;
+
+        redVal = calculateVal(stepR, redVal, loopCount);
+        grnVal = calculateVal(stepG, grnVal, loopCount);
+        bluVal = calculateVal(stepB, bluVal, loopCount);
+
+        setColor(redVal, grnVal, bluVal); // Write current values to LED pins
+
+        Serial.print("Loop count: ");
+        Serial.println(loopCount);
+        loopCount++;
+      }
+      else {
+        inFade = false;
+      }
+    }
+  }
+}
+
+
+
 /************ Main Loop ******************/
 long lastReconnectAttempt = 0;
 void loop() {
@@ -359,4 +408,72 @@ void loop() {
   } else {
     client.loop();
   }
+
+  handleCrossfade();
+}
+
+
+
+// From https://www.arduino.cc/en/Tutorial/ColorCrossfader
+/* BELOW THIS LINE IS THE MATH -- YOU SHOULDN'T NEED TO CHANGE THIS FOR THE BASICS
+*
+* The program works like this:
+* Imagine a crossfade that moves the red LED from 0-10,
+*   the green from 0-5, and the blue from 10 to 7, in
+*   ten steps.
+*   We'd want to count the 10 steps and increase or
+*   decrease color values in evenly stepped increments.
+*   Imagine a + indicates raising a value by 1, and a -
+*   equals lowering it. Our 10 step fade would look like:
+*
+*   1 2 3 4 5 6 7 8 9 10
+* R + + + + + + + + + +
+* G   +   +   +   +   +
+* B     -     -     -
+*
+* The red rises from 0 to 10 in ten steps, the green from
+* 0-5 in 5 steps, and the blue falls from 10 to 7 in three steps.
+*
+* In the real program, the color percentages are converted to
+* 0-255 values, and there are 1020 steps (255*4).
+*
+* To figure out how big a step there should be between one up- or
+* down-tick of one of the LED values, we call calculateStep(),
+* which calculates the absolute gap between the start and end values,
+* and then divides that gap by 1020 to determine the size of the step
+* between adjustments in the value.
+*/
+int calculateStep(int prevValue, int endValue) {
+    int step = endValue - prevValue; // What's the overall gap?
+    if (step) {                      // If its non-zero,
+        step = 1020/step;            //   divide by 1020
+    }
+
+    return step;
+}
+
+/* The next function is calculateVal. When the loop value, i,
+*  reaches the step size appropriate for one of the
+*  colors, it increases or decreases the value of that color by 1.
+*  (R, G, and B are each calculated separately.)
+*/
+int calculateVal(int step, int val, int i) {
+    if ((step) && i % step == 0) { // If step is non-zero and its time to change a value,
+        if (step > 0) {              //   increment the value if step is positive...
+            val += 1;
+        }
+        else if (step < 0) {         //   ...or decrement it if step is negative
+            val -= 1;
+        }
+    }
+
+    // Defensive driving: make sure val stays in the range 0-255
+    if (val > 255) {
+        val = 255;
+    }
+    else if (val < 0) {
+        val = 0;
+    }
+
+    return val;
 }

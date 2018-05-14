928f11d8ab4e7dc5a0e033da7a1bdd356f8c8441
Nick Rook <rooknj@outlook.com>
2018-05-13 21:02:26 -0700

    Fixed bug with Juggle

--- LightController/LightController.ino
+++ LightController/LightController.ino
@@ -683,7 +683,7 @@ void handleJuggle() {
     fadeToBlackBy( leds, NUM_LEDS, 20);
     byte dothue = 0;
     for( int i = 0; i < 8; i++) {
-        leds[beatsin16(i+7,0,NUM_LEDS)] |= CHSV(dothue, 200, 255);
+        leds[beatsin16(i+7,0,NUM_LEDS-1)] |= CHSV(dothue, 200, 255);
         dothue += 32;
     }
     FastLED.show();

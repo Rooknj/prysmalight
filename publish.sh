dea63778a21dc19680ed1d1548bb937fa47245b5
Nick Rook <nick_rook@intuit.com>
2018-05-17 14:19:49 -0700

    Edited publish file

--- publish.sh
+++ publish.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 printf "Enter the tag for your image: "
 read TAG
-printf "I will tag this docker image with \"$TAG\", is that ok? [Y/n]: " 
+printf "I will tag this docker image with \"broker-$TAG\", is that ok? [Y/n]: " 
 read CONFIRM
 
 if [ "$CONFIRM" = "Y" ] ; then
     echo "Building docker image"
     docker-compose build
 
-    echo "Tagging docker image with \"$TAG\""
-    docker tag lightapp2-broker-prod rooknj/lightapp2:$TAG
+    echo "Tagging docker image with \"broker-$TAG\""
+    docker tag lightapp2-broker-prod rooknj/lightapp2:broker-$TAG
 
     echo "Pushing docker image"
-    docker push rooknj/lightapp2:$TAG
+    docker push rooknj/lightapp2:broker-$TAG
 else
     echo "Aborting"
 fi

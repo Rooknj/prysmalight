e342d7c2e7ca3c0acfbcc9d49f98e3ef5670f1b4
Nick Rook <nick_rook@intuit.com>
2018-07-14 11:32:55 -0700

    added unit tests to subscription container

--- src/Components/LightList/LightListSubscriptionContainer.test.js
+++ src/Components/LightList/LightListSubscriptionContainer.test.js
@@ -5,3 +5,18 @@ import LightListSubscriptionContainer from "./LightListSubscriptionContainer";
 it("renders without crashing", () => {
     shallow(<LightListSubscriptionContainer />);
 });
+
+it("subscribes to light changes on mount", () => {
+    // Set up mocked functions
+    const subscribeToLightChangesSpy = jest.fn();
+
+    // Render the container with the mock function
+    shallow(
+        <LightListSubscriptionContainer
+            subscribeToLightChanges={subscribeToLightChangesSpy}
+        />
+    );
+
+    // Expect the subscription function to be called once
+    expect(subscribeToLightChangesSpy).toHaveBeenCalledTimes(1);
+});

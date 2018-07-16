2ad75f39482ea47f5502a3750b88c5982d391415
Nick Rook <nick_rook@intuit.com>
2018-07-15 18:43:52 -0700

    Styled LightStatus avatar

--- src/Components/LightList/Light/LightHeader/LightStatus.jsx
+++ src/Components/LightList/Light/LightHeader/LightStatus.jsx
@@ -3,14 +3,12 @@ import PropTypes from "prop-types";
 import CardHeader from "@material-ui/core/CardHeader";
 import Avatar from "@material-ui/core/Avatar";
 import HightlightIcon from "@material-ui/icons/Highlight";
-import { withStyles } from "@material-ui/core/styles";
 import grey from "@material-ui/core/colors/grey";
+import styled from "styled-components";
 
-const styles = theme => ({
-    avatar: {
-        backgroundColor: grey[400]
-    }
-});
+const StyledAvatar = styled.div`
+    backgroundcolor: ${grey[400]};
+`;
 
 const propTypes = {
     id: PropTypes.string.isRequired,
@@ -37,13 +35,15 @@ const LightStatus = props => {
             title={props.id}
             subheader={props.connected === 2 ? "Connected" : "Disonnected"}
             avatar={
-                <Avatar aria-label="Light" className={props.classes.avatar}>
-                    <HightlightIcon
-                        nativeColor={`rgb(${props.color.r},${props.color.g},${
-                            props.color.b
-                        })`}
-                    />
-                </Avatar>
+                <StyledAvatar>
+                    <Avatar aria-label="Light" className={props.classes.avatar}>
+                        <HightlightIcon
+                            nativeColor={`rgb(${props.color.r},${
+                                props.color.g
+                            },${props.color.b})`}
+                        />
+                    </Avatar>
+                </StyledAvatar>
             }
         />
     );
@@ -52,4 +52,4 @@ const LightStatus = props => {
 LightStatus.propTypes = propTypes;
 LightStatus.defaultProps = defaultProps;
 
-export default withStyles(styles)(LightStatus);
+export default LightStatus;

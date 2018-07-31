d2d4e5bf385ca1a81d42853879b5110cb221f746
Nick Rook <nick_rook@intuit.com>
2018-07-31 13:20:58 -0700

    Revert "added dockerScripts to gulp"
    This reverts commit d6ed010030b719ab2754fde7b2e6dbe7062e0f75.

--- gulpfile.js
+++ gulpfile.js
@@ -170,9 +170,5 @@ const makePkg = async () => {
 };
 gulp.task("build", gulp.series("set-prod", "babel", makePkg));
 
-gulp.task("buildDockerImage", run("node scripts/dockerScripts build"))
-
-gulp.task("publishDockerImage", run(["node scripts/dockerScripts tag", "node scripts/dockerScripts publish"]))
-
 // DEFAULT: TBD
 gulp.task("default", gulp.series("lint", "babel"));

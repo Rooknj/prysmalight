c1c2f5d89786c89b001801fcb4d718f55098d0e5
Nick Rook <rooknj@outlook.com>
2018-08-05 09:50:10 -0700

    fixed yarn clean

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,9 +1,17 @@
 const { execSync } = require("child_process");
+const rimraf = require("rimraf");
 
+// Clear redis database
 try {
-  execSync("redis-cli flushall");
+  execSync("redis-cli flushall", { timeout: 2000 });
 } catch (error) {
-  console.log("Redis is not connected")
+  console.log("Redis is not connected");
 }
-execSync("rm -rf build");
+
+// Remove build folder
+rimraf("build", function(error) {
+  if(error) console.log("Error on rimraf: ", error);
+});
+
+// Bring down docker containers
 execSync("docker-compose down");

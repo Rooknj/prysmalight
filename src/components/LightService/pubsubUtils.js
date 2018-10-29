ebd27e85d71818b83faed9cf99fdd13ca5a1bf36
Nick Rook <rooknj@outlook.com>
2018-10-29 14:01:25 -0400

    initialized pubsubUtils factory

---
+++ src/components/LightService/pubsubUtils.js
@@ -0,0 +1,29 @@
+//mqtt.Client#connected
+
+const MQTT = require("async-mqtt");
+const Debug = require("debug").default;
+const { parseMqttMessage } = require("./lightUtil");
+const debug = Debug("pubsubUtils");
+
+/**
+ * Factory which returns an object with all database methods
+ * @param {object} client - The Redis client
+ */
+const pubsubUtilsFactory = client => {
+  return Object.create({});
+};
+
+/**
+ * Checks to make sure a client was provided. If not, rejects with an error
+ * @param {object} client - The MQTT client
+ */
+const connect = client => {
+  return new Promise((resolve, reject) => {
+    if (!client) {
+      reject(new Error("Client not supplied!"));
+    }
+    resolve(pubsubUtilsFactory(client));
+  });
+};
+
+module.exports = connect;

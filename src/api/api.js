9859e2a98132a9ab60d177abacfbae83ab9095e2
Nick Rook <rooknj@outlook.com>
2018-10-29 21:29:19 -0400

    added a few comments

--- src/api/api.js
+++ src/api/api.js
@@ -4,10 +4,15 @@ const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
 const makeApolloServer = (app, options) => {
+  // Generate the apolloServer and apply the middleware to the express app
   const apolloServer = new ApolloServer({ typeDefs, resolvers });
   apolloServer.applyMiddleware({ app });
+
+  // Create an httpServer using the apolloServer and add subscription support
   const httpServer = http.createServer(app);
   apolloServer.installSubscriptionHandlers(httpServer);
+
+  // Pass along some data
   httpServer.graphqlPath = apolloServer.graphqlPath;
   httpServer.subscriptionsPath = apolloServer.subscriptionsPath;
   return httpServer;

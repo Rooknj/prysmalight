a75e9644c1ad9792457054c7a629a5e53b94326c
Nick Rook <rooknj@outlook.com>
2018-11-03 17:50:24 -0400

    added dbFactory test template

---
+++ src/repository/dbFactory.test.js
@@ -0,0 +1,35 @@
+const dbFactory = require("./dbFactory");
+
+describe("connections", () => {
+  test("has a port defined", () => {});
+});
+
+describe("disconnections", () => {
+  test("has a port defined", () => {});
+
+  test("has a host defined", () => {});
+});
+
+describe("getLight", () => {
+  test("TODO", () => {});
+});
+
+describe("getAllLights", () => {
+  test("TODO", () => {});
+});
+
+describe("setLight", () => {
+  test("TODO", () => {});
+});
+
+describe("addLight", () => {
+  test("TODO", () => {});
+});
+
+describe("removeLight", () => {
+  test("TODO", () => {});
+});
+
+describe("hasLight", () => {
+  test("TODO", () => {});
+});

a1d39323d212da954d9a81d439c6f6383d5a3b9a
Nick Rook <rooknj@outlook.com>
2018-11-26 19:37:52 -0500

    removed redis-cli dependency

--- scripts/integration.js
+++ scripts/integration.js
@@ -13,23 +13,58 @@ process.on("unhandledRejection", err => {
 
 const { execSync } = require("child_process");
 const jest = require("jest");
-let argv = process.argv.slice(2);
 
-// Add the test/integration/ argument to the beginning of the array
-// Makes jest only run the unit tests inside of the src/ folder
-argv.unshift("test/integration/");
+const testIntegration = async argv => {
+  // Add the test/integration/ argument to the beginning of the array
+  // Makes jest only run the unit tests inside of the src/ folder
+  argv.unshift("test/integration/");
 
-// Bring up server, redis, and broker
+  console.log(
+    "Note: This program tests against the MOST RECENTLY BUILT server image. This means that the first time running it on a new machine will take a while so it can build the image. Make sure to rebuild using docker-compose build every time you make a change and want to test it."
+  );
+  console.log("");
 
-if (argv.indexOf("--rpi") >= 0) {
-  argv.splice(argv.indexOf("--rpi"), 1); // Remove rpi option from argv so jest doesnt screw up
-  console.log("Bringing up Raspberry Pi server, broker, and redis");
-  execSync("docker-compose -f docker-compose.rpi.yml up -d");
-} else {
-  console.log("Bringing up server, broker, and redis");
-  execSync("docker-compose up -d");
-}
+  // Bring up server, redis, and broker
+  if (argv.indexOf("--rpi") >= 0) {
+    argv.splice(argv.indexOf("--rpi"), 1); // Remove rpi option from argv so jest doesnt screw up
+    console.log("Bringing up Raspberry Pi server, broker, and redis");
+    execSync("docker-compose -f docker-compose.rpi.yml up -d");
+  } else {
+    console.log("Bringing up server, broker, and redis");
+    execSync("docker-compose up -d");
+  }
 
-execSync("redis-cli flushall");
-jest.run(argv);
+  // Connect to redis for clearing it before we test
+  const { promisify } = require("util");
+  const config = require("../src/config/config");
+  const redis = require("redis");
+  const redisClient = redis.createClient(
+    config.redisSettings.port,
+    config.redisSettings.host
+  );
 
+  // If redis fails to connect, just quit all connection attempts
+  redisClient.on("error", err => {
+    console.log(err);
+    redisClient.quit();
+  });
+
+  // Attempt to clear redis database then run tests if successful
+  const asyncFLUSHALL = promisify(redisClient.FLUSHALL).bind(redisClient);
+
+  // Clear redis database
+  try {
+    const reply = await asyncFLUSHALL();
+    console.log("Successfully cleared redis", reply);
+    console.log("Running Integration Tests");
+    jest.run(argv);
+  } catch (error) {
+    console.log("Did not clear redis. Aborting");
+  }
+  
+  // End redis connection
+  redisClient.quit();
+};
+
+let args = process.argv.slice(2);
+testIntegration(args);

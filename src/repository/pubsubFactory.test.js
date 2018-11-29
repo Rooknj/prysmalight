c9e87d618c5b2a2a44f50b369c1858731d9cbc10
Nick Rook <rooknj@outlook.com>
2018-11-29 14:59:43 -0500

    added pubsubFactory tests

--- src/repository/pubsubFactory.test.js
+++ src/repository/pubsubFactory.test.js
@@ -13,7 +13,9 @@ const MQTT_EFFECT_LIST_TOPIC = mqttSettings.MQTT_EFFECT_LIST_TOPIC;
 const createMockClient = () => {
   return {
     on: () => {},
-    connected: true
+    subscribe: jest.fn(async topic => [{ topic, qos: 0 }]),
+    publish: jest.fn(async (topic, payload) => {}),
+    unsubscribe: jest.fn(async topic => {})
   };
 };
 
@@ -110,13 +112,284 @@ describe("effectMessages", () => {
 });
 
 describe("subscribeToLight", () => {
-  test("returns an error if the client is not connected", () => {});
+  test("Subscribes to all the correct topics (Example 1)", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = "Test A";
+
+    // Call The Method
+    const error = await pubsub.subscribeToLight(ID);
+
+    // Test
+    expect(error).toBeNull();
+    expect(mockClient.subscribe).toBeCalledTimes(3);
+    expect(mockClient.subscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_LIGHT_CONNECTED_TOPIC}`
+    );
+    expect(mockClient.subscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_LIGHT_STATE_TOPIC}`
+    );
+    expect(mockClient.subscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_EFFECT_LIST_TOPIC}`
+    );
+  });
+  test("Subscribes to all the correct topics (Example 2)", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = "Test 123";
+
+    // Call The Method
+    const error = await pubsub.subscribeToLight(ID);
+
+    // Test
+    expect(error).toBeNull();
+    expect(mockClient.subscribe).toBeCalledTimes(3);
+    expect(mockClient.subscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_LIGHT_CONNECTED_TOPIC}`
+    );
+    expect(mockClient.subscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_LIGHT_STATE_TOPIC}`
+    );
+    expect(mockClient.subscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_EFFECT_LIST_TOPIC}`
+    );
+  });
+  test("returns an error if the client is not connected", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = false;
+    const ID = "Test A";
+
+    // Call The Method
+    const error = await pubsub.subscribeToLight(ID);
+
+    // Test
+    expect(error).toBeInstanceOf(Error);
+  });
+  test("returns an error if it fails to subscribe to at least one topic", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    mockClient.subscribe = jest.fn(async () => {
+      throw new Error();
+    });
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = "Test A";
+
+    // Call The Method
+    const error = await pubsub.subscribeToLight(ID);
+
+    // Test
+    expect(error).toBeInstanceOf(Error);
+    expect(mockClient.subscribe).toBeCalled();
+  });
+  test("returns an error if no id was provided", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+
+    // Call The Method
+    const error = await pubsub.subscribeToLight();
+
+    // Test
+    expect(error).toBeInstanceOf(Error);
+  });
 });
 
 describe("publishToLight", () => {
-  test("returns an error if the client is not connected", () => {});
+  test("Publishes the message to the correct topic as a buffer (Example 1)", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = "Test A";
+    const MESSAGE = { brightness: 40 };
+
+    // Call The Method
+    const error = await pubsub.publishToLight(ID, MESSAGE);
+
+    // Test
+    expect(error).toBeNull();
+    expect(mockClient.publish).toBeCalledTimes(1);
+    expect(mockClient.publish).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_LIGHT_COMMAND_TOPIC}`,
+      Buffer.from(JSON.stringify(MESSAGE))
+    );
+  });
+  test("Publishes the message to the correct topic as a buffer (Example 2)", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = "Test A";
+    const MESSAGE = { effect: "Cylon" };
+
+    // Call The Method
+    const error = await pubsub.publishToLight(ID, MESSAGE);
+
+    // Test
+    expect(error).toBeNull();
+    expect(mockClient.publish).toBeCalledTimes(1);
+    expect(mockClient.publish).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_LIGHT_COMMAND_TOPIC}`,
+      Buffer.from(JSON.stringify(MESSAGE))
+    );
+  });
+  test("returns an error if the client is not connected", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = false;
+    const ID = "Test A";
+    const MESSAGE = { effect: "Cylon" };
+
+    // Call The Method
+    const error = await pubsub.publishToLight(ID, MESSAGE);
+
+    // Test
+    expect(error).toBeInstanceOf(Error);
+  });
+  test("returns an error if the client fails to publish", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    mockClient.publish = jest.fn(() => {
+      throw new Error();
+    });
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = "Test A";
+    const MESSAGE = { effect: "Cylon" };
+
+    // Call The Method
+    const error = await pubsub.publishToLight(ID, MESSAGE);
+
+    // Test
+    expect(error).toBeInstanceOf(Error);
+    expect(mockClient.publish).toBeCalled();
+  });
+  test("returns an error if no id was provided", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = null;
+    const MESSAGE = { effect: "Cylon" };
+
+    // Call The Method
+    const error = await pubsub.publishToLight(ID, MESSAGE);
+
+    // Test
+    expect(error).toBeInstanceOf(Error);
+  });
+  test("returns an error if no message was provided", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = "Test A";
+    const MESSAGE = null;
+
+    // Call The Method
+    const error = await pubsub.publishToLight(ID, MESSAGE);
+
+    // Test
+    expect(error).toBeInstanceOf(Error);
+  });
 });
 
 describe("unsubscribeFromLight", () => {
-  test("returns an error if the client is not connected", () => {});
+  test("Unsubscribes from all the correct topics (Example 1)", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = "Test A";
+
+    // Call The Method
+    const error = await pubsub.unsubscribeFromLight(ID);
+
+    // Test
+    expect(error).toBeNull();
+    expect(mockClient.unsubscribe).toBeCalledTimes(3);
+    expect(mockClient.unsubscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_LIGHT_CONNECTED_TOPIC}`
+    );
+    expect(mockClient.unsubscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_LIGHT_STATE_TOPIC}`
+    );
+    expect(mockClient.unsubscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_EFFECT_LIST_TOPIC}`
+    );
+  });
+  test("Unsubscribes from all the correct topics (Example 2)", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = "Test 123";
+
+    // Call The Method
+    const error = await pubsub.unsubscribeFromLight(ID);
+
+    // Test
+    expect(error).toBeNull();
+    expect(mockClient.unsubscribe).toBeCalledTimes(3);
+    expect(mockClient.unsubscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_LIGHT_CONNECTED_TOPIC}`
+    );
+    expect(mockClient.unsubscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_LIGHT_STATE_TOPIC}`
+    );
+    expect(mockClient.unsubscribe).toBeCalledWith(
+      `${MQTT_LIGHT_TOP_LEVEL}/${ID}/${MQTT_EFFECT_LIST_TOPIC}`
+    );
+  });
+  test("returns an error if the client is not connected", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = false;
+    const ID = "Test A";
+
+    // Call The Method
+    const error = await pubsub.unsubscribeFromLight(ID);
+
+    // Test
+    expect(error).toBeInstanceOf(Error);
+  });
+  test("returns an error if it fails to unsubscribe from at least one topic", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    mockClient.unsubscribe = jest.fn(async () => {
+      throw new Error();
+    });
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+    const ID = "Test A";
+
+    // Call The Method
+    const error = await pubsub.unsubscribeFromLight(ID);
+
+    // Test
+    expect(error).toBeInstanceOf(Error);
+    expect(mockClient.unsubscribe).toBeCalled();
+  });
+  test("returns an error if no id was provided", async () => {
+    // Create the client and pubsub
+    let mockClient = createMockClient();
+    const pubsub = pubsubFactory(mockClient);
+    pubsub.__proto__.connected = true;
+
+    // Call The Method
+    const error = await pubsub.unsubscribeFromLight();
+
+    // Test
+    expect(error).toBeInstanceOf(Error);
+  });
 });

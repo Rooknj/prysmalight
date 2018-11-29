a4a7f3131031771914e2fedeeac9c4926558fbe4
Nick Rook <rooknj@outlook.com>
2018-11-29 15:14:15 -0500

    initialized repository test

---
+++ src/repository/repository.test.js
@@ -0,0 +1,10 @@
+// Creates a mock db
+const createMockDependencies = () => {
+  let mockDB = {};
+  let mockPubsub = {};
+  return { db: mockDB, pubsub: mockPubsub };
+};
+
+describe("connections", () => {
+  test("Test", () => {});
+});

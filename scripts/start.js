8f43e58ea2f72f2761fc37661d8985e1f12950e7
Nick Rook <rooknj@outlook.com>
2018-12-04 23:50:40 -0500

    broke down into just a mock service

--- scripts/start.js
+++ scripts/start.js
@@ -3,7 +3,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = "development";
 process.env.NODE_ENV = "development";
-process.env.DEBUG = "main,config,server,MockLight,db,pubsub,repo";
+process.env.DEBUG = "main,config,server";
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -12,24 +12,17 @@ process.on("unhandledRejection", err => {
   throw err;
 });
 
-const { execSync } = require("child_process");
+//const { execSync } = require("child_process");
 const nodemon = require("nodemon");
 
 let argv = process.argv.slice(2);
 
 if (argv.indexOf("--mock") >= 0) {
-  process.env.MOCK = true;
+  console.log("Mock Flag Set");
 }
 
 if (argv.indexOf("--local") >= 0) {
-  console.log("Spinning up Local MQTT broker");
-  process.env.MQTT_HOST = "localhost";
-  execSync("docker-compose up -d broker");
-}
-
-if (!process.env.MOCK) {
-  console.log("Spinning up Local Redis Server");
-  execSync("docker-compose up -d redis");
+  console.log("Local flag set");
 }
 
 // TODO: Figure out how to get rid of that error that pops up.
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 const config = require("./config/config");
 const server = require("./server/server");
-const dbFactory = require("./repository/dbFactory");
-const pubsubFactory = require("./repository/pubsubFactory");
-const repository = require("./repository/repository");
-const MockLight = require("./mock/MockLight");
 const Debug = require("debug").default;
 const debug = Debug("main");
 
 // Verbose statement of service starting
-debug("--- Light Service ---");
+debug("--- API Gateway Microservice ---");
 
 // Unhandled error logging
 process.on("uncaughtException", err => {
@@ -18,46 +14,8 @@ process.on("uncaughtRejection", err => {
   debug("Unhandled Rejection", err);
 });
 
-const getRepo = () => {
-  const mockRepository = require("./mock/mockRepository");
-  if (process.env.MOCK) return mockRepository;
-
-  // Create all dependencies
-  const redis = require("redis");
-  const dbClient = redis.createClient(
-    config.redisSettings.port,
-    config.redisSettings.host
-  );
-  // TODO: add the MQTT Topics as a part of dependency injection
-  const mqtt = require("async-mqtt");
-  const pubsubClient = mqtt.connect(
-    config.mqttSettings.host,
-    {
-      reconnectPeriod: config.mqttSettings.reconnectPeriod, // Amount of time between reconnection attempts
-      username: config.mqttSettings.username,
-      password: config.mqttSettings.password
-    }
-  );
-
-  //TODO: Include this stuff in deps
-  const { PubSub } = require("apollo-server");
-  const gqlPubSub = new PubSub();
-
-  // TODO: Figure out where to put this
-  const events = require("events");
-  const event = new events.EventEmitter();
-
-  // Create our db and pubsub with the provided clients
-  const db = dbFactory(dbClient);
-  const pubsub = pubsubFactory(pubsubClient);
-
-  // Inject Dependencies
-  return repository({ db, pubsub, gqlPubSub, event });
-};
-
-// Start the server
-const repo = getRepo();
-repo.init();
+const mockRepository = require("./mock/mockRepository");
+const repo = mockRepository;
 debug("Starting Server");
 server
   .start({
@@ -69,31 +27,3 @@ server
       debug("App Closed");
     });
   });
-
-// Create a Default Mock Light
-const createMockLight = mockName => {
-  debug(`Starting mock light: ${mockName}`);
-  const mockLight = new MockLight(mockName);
-  mockLight.subscribeToCommands();
-  mockLight.publishConnected({ name: mockName, connection: 2 });
-  mockLight.publishEffectList({
-    name: mockName,
-    effectList: ["Test 1", "Test 2", "Test 3"]
-  });
-  mockLight.publishState({
-    name: mockName,
-    state: "OFF",
-    color: { r: 255, g: 100, b: 0 },
-    brightness: 100,
-    effect: "None",
-    speed: 4
-  });
-  debug(`${mockName} Ready`);
-};
-createMockLight("Default Mock");
-
-// Set up any extra mock lights if the environment dictates it
-if (process.env.MOCKS) {
-  const mockArray = process.env.MOCKS.split(",");
-  mockArray.forEach(createMockLight);
-}

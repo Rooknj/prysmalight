b942a03a570e8d2aad21c7a931f1695314ff0be6
Nick Rook <rooknj@outlook.com>
2018-12-16 21:51:50 -0500

    adde outher build files

--- packages/api/deploy.sh
+++ packages/api/deploy.sh
@@ -1,1 +1,13 @@
 echo "deploying api"
+
+deploy() {
+  node scripts/dockerScripts tag 
+  node scripts/dockerScripts publish
+}
+
+if [ "$TRAVIS" == "true" ]; then
+  deploy
+else
+  echo "Not in CI pipeline, aborting"
+  exit 1
+fi

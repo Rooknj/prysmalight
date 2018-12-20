3ad49e9785daa3ed70ff2c7daf69561b15e1ef7a
Nick Rook <rooknj@outlook.com>
2018-12-20 18:47:21 -0500

    added a light dialog popup

--- packages/client/src/Components/LightList-old/Light/LightMutationContainer.test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import LightMutationContainer from "./LightMutationContainer";
-
-it("renders without crashing", () => {
-  shallow(<LightMutationContainer />);
-});

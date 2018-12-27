9be5395f1c7a0bff651ca36d6fc9fef98313a47c
Nick Rook <rooknj@outlook.com>
2018-12-27 13:39:02 -0500

    fixed unit tests

--- packages/client/src/Components/LightList/Light/BrightnessSlider/BrightnessSlider.test.js
+++ packages/client/src/Components/LightList/Light/BrightnessSlider/BrightnessSlider.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import BrightnessSlider from "./BrightnessSlider.test";
+import BrightnessSlider from "./BrightnessSlider";
 
 it("renders without crashing", () => {
   shallow(<BrightnessSlider />);
--- packages/client/src/Components/LightList/Light/LightDialog/ComplexLight.test.js
+++ packages/client/src/Components/LightList/Light/LightDialog/ComplexLight.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
-import Light from "../LightDialog";
+import Light from "./ComplexLight";
 import { createSerializer } from "enzyme-to-json";
 import { ThemeProvider } from "styled-components";
 

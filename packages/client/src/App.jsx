41b45b042d74165a8d635f37d288157d7f653b68
Nick Rook <rooknj@outlook.com>
2019-01-05 15:45:06 -0500

    Put Styled-Components style before

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -5,16 +5,31 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
 import { ThemeProvider } from "styled-components";
+import JssProvider from "react-jss/lib/JssProvider";
+import { create } from "jss";
+import { createGenerateClassName, jssPreset } from "@material-ui/core/styles";
+
+/**
+ * This is here in order to put Styled Components' CSS rules above the default Material UI rules
+ */
+const generateClassName = createGenerateClassName();
+const jss = create({
+  ...jssPreset(),
+  // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
+  insertionPoint: document.getElementById("jss-insertion-point")
+});
 
 const App = () => (
-  <MuiThemeProvider theme={theme}>
-    <ThemeProvider theme={{ ...theme }}>
-      <CssBaseline>
-        <AppBar />
-        <LightTool />
-      </CssBaseline>
-    </ThemeProvider>
-  </MuiThemeProvider>
+  <JssProvider jss={jss} generateClassName={generateClassName}>
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={{ ...theme }}>
+        <CssBaseline>
+          <AppBar />
+          <LightTool />
+        </CssBaseline>
+      </ThemeProvider>
+    </MuiThemeProvider>
+  </JssProvider>
 );
 
 export default App;

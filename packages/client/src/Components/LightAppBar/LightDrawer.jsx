cff30b224580e17ae04887e71bae26d8ca5d285f
Nick Rook <rooknj@outlook.com>
2019-01-22 20:26:37 -0500

    added a drawer header

--- packages/client/src/Components/LightAppBar/LightDrawer.jsx
+++ packages/client/src/Components/LightAppBar/LightDrawer.jsx
@@ -6,9 +6,20 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import styled from "styled-components";
+
+const DrawerHeader = styled.div`
+  @media (min-width: 600px) {
+    min-height: 64px;
+  }
+  min-height: 56px;
+`;
 
 const LightDrawer = ({ open, onOpen, onClose }) => (
   <SwipeableDrawer anchor="left" open={open} onOpen={onOpen} onClose={onClose}>
+    <DrawerHeader />
+    <Divider />
     <List>
       <ListItem button>
         <ListItemIcon>

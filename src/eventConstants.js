a8112ca65d72a5976f6925ec54df0512bcdb6f40
Nick Rook <rooknj@outlook.com>
2019-02-07 21:40:26 -0500

    added list of event constants

---
+++ src/eventConstants.js
@@ -0,0 +1,8 @@
+export const GET_LIGHT = "getLight";
+export const GET_LIGHTS = "getLights";
+export const SET_LIGHT = "setLight";
+export const ADD_LIGHT = "addLight";
+export const REMOVE_LIGHT = "removeLight";
+export const LIGHT_ADDED = "lightAdded";
+export const LIGHT_REMOVED = "lightRemoved";
+export const LIGHT_CHANGED = "lightChanged";

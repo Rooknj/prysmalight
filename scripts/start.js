549f712a86b21d4488f9fe0f6b821bf08fb2afda
Nick Rook <rooknj@outlook.com>
2019-02-11 13:01:10 -0500

    upgraded start script

--- scripts/start.js
+++ scripts/start.js
@@ -1,36 +1,46 @@
 "use strict";
 
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = "development";
 process.env.NODE_ENV = "development";
 process.env.DEBUG = "main,config,server,service,db,pubsub,repo";
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+const spawn = require("cross-spawn");
+const spawnArgs = require("spawn-args");
+const { delimiter } = require("path");
+const pathResolve = require("path").resolve;
+const { execSync } = require("child_process");
+
+// Crash on unhandled rejections
 process.on("unhandledRejection", err => {
   throw err;
 });
 
-const { execSync } = require("child_process");
-const nodemon = require("nodemon");
-
+// Get start arguments
 let argv = process.argv.slice(2);
 
+// Start docker containers
 console.log("Spinning up Local MQTT broker");
 process.env.MQTT_HOST = "localhost";
 console.log("Spinning up Local Redis Server");
 process.env.REDIS_HOST = "localhost";
-
-// Start docker containers
 execSync("docker-compose up -d broker redis", {
   stdio: [process.stdin, process.stdout] // Ignore stderr so nothing prints to the console if this fails.
 });
 
+// Handle mock server
 if (argv.indexOf("--mock") >= 0) {
   console.log("Starting Mock Server");
   process.env.MOCK = true;
 }
 
-// TODO: Figure out how to get rid of that error that pops up.
-nodemon(".");
+// Start Nodemon with cross-spawn
+const args = spawnArgs("nodemon", { removequotes: "always" });
+spawn.sync(args.shift(), args, {
+  stdio: ["inherit", "inherit", "inherit"],
+  cwd: process.cwd(),
+  env: Object.assign({}, process.env, {
+    PATH:
+      process.env.PATH +
+      delimiter +
+      pathResolve(process.cwd(), "node_modules", ".bin")
+  })
+});

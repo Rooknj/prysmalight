2e8d2098dbd0ace0904054c657c9d65a9743029b
Nick Rook <rooknj@outlook.com>
2019-02-17 19:12:55 -0500

    started start method

--- src/discovery/discovery.js
+++ src/discovery/discovery.js
@@ -1,12 +1,37 @@
 const createDiscoveryService = (mediator, mqttClient) => {
   let self;
 
-  const start = () => {
+  const start = async () => {
     console.log("Start Discovery");
+    //message
+    mqttClient.on("message", console.log);
+
+    //pub
+    try {
+      const granted = await mqttClient.subscribe("prysmalight/+/config");
+      if (!granted[0]) {
+        console.log(`Subscription not granted`);
+      } else {
+        console.log(
+          `Subscribed to ${granted[0].topic} with a qos of ${granted[0].qos}`
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    //sub
+    try {
+      await mqttClient.publish("prysmalight/discovery", "Hello");
+      console.log(`Published hello to discovery`);
+    } catch (error) {
+      return error;
+    }
   };
 
   const stop = () => {
     console.log("Stop Discovery");
+    //client.unsubscribe(topic);
   };
 
   const getDiscoveredLights = () => {};

3f137145c06b290812c172e1977d24a78cec3dbe
Nick Rook <rooknj@outlook.com>
2019-02-20 16:50:54 -0500

    added remote start

--- scripts/start.js
+++ scripts/start.js
@@ -17,19 +17,23 @@ process.on("unhandledRejection", err => {
 // Get start arguments
 let argv = process.argv.slice(2);
 
-// Start docker containers
-console.log("Spinning up Local MQTT broker");
-process.env.MQTT_HOST = "localhost";
-console.log("Spinning up Local Redis Server");
-process.env.REDIS_HOST = "localhost";
-execSync("docker-compose up -d broker redis", {
-  stdio: [process.stdin, process.stdout] // Ignore stderr so nothing prints to the console if this fails.
-});
-
 // Handle mock server
 if (argv.indexOf("--mock") >= 0) {
   console.log("Starting Mock Server");
   process.env.MOCK = true;
+} else if (argv.indexOf("--remote") >= 0) {
+  console.log("Starting Server with Remote services");
+  process.env.MQTT_HOST = "prysma.local";
+  process.env.REDIS_HOST = "prysma.local";
+} else {
+  // Start docker containers
+  console.log("Spinning up Local MQTT broker");
+  process.env.MQTT_HOST = "localhost";
+  console.log("Spinning up Local Redis Server");
+  process.env.REDIS_HOST = "localhost";
+  execSync("docker-compose up -d broker redis", {
+    stdio: [process.stdin, process.stdout] // Ignore stderr so nothing prints to the console if this fails.
+  });
 }
 
 // Start Nodemon with cross-spawn

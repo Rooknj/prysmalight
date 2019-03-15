2b146c53d9e033ca036e84f4d526dabec994b63b
Nick Rook <rooknj@outlook.com>
2019-03-14 23:18:03 -0400

    removed createLightService from index

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@
 
 const config = require("./config");
 const Server = require("./server/server");
-const createLightService = require("./lightService");
+const LightService = require("./lightService");
 const MockLight = require("./mock/MockLight");
-const mediator = require("./mediator/mediator");
-const dbFactory = require("./lightService/dbFactory");
-const pubsubFactory = require("./lightService/pubsubFactory");
 
 // Verbose statement of service starting
 console.log("--- Prysmalight ---");
@@ -21,52 +18,36 @@ process.on("uncaughtRejection", err => {
   console.log("Unhandled Rejection", err);
 });
 
-// Start the GraphQL server
-const startServer = async () => {
-  // Create a redis client
-  const redisClient = config.redis.connect(config.redisSettings);
+// Initialize the light service
+const lightService = LightService();
+lightService.init();
 
-  // Create an MQTT client
-  const mqttClient = config.mqtt.connect(config.mqttSettings);
+// Start the Default Mock Light
+const createMockLight = async mockName => {
+  console.log(`Starting mock light: ${mockName}`);
+  const mockLight = new MockLight(mockName);
+  mockLight.subscribeToCommands();
+  mockLight.publishConnected({ name: mockName, connection: 2 });
+  mockLight.publishEffectList({
+    name: mockName,
+    effectList: ["Test 1", "Test 2", "Test 3"]
+  });
+  mockLight.publishState({
+    name: mockName,
+    state: "OFF",
+    color: { r: 255, g: 100, b: 0 },
+    brightness: 100,
+    effect: "None",
+    speed: 4
+  });
+  console.log(`${mockName} Ready`);
+};
+createMockLight("Default Mock");
 
-  const db = dbFactory(redisClient);
-  const pubsub = pubsubFactory({ client: mqttClient, mediator });
-
-  // Start the lightService
-  const lightService = createLightService({ mediator, db, pubsub });
-  lightService.init();
-
-  // Start the Default Mock Light
-  const createMockLight = async mockName => {
-    console.log(`Starting mock light: ${mockName}`);
-    const mockLight = new MockLight(mockName);
-    mockLight.subscribeToCommands();
-    mockLight.publishConnected({ name: mockName, connection: 2 });
-    mockLight.publishEffectList({
-      name: mockName,
-      effectList: ["Test 1", "Test 2", "Test 3"]
-    });
-    mockLight.publishState({
-      name: mockName,
-      state: "OFF",
-      color: { r: 255, g: 100, b: 0 },
-      brightness: 100,
-      effect: "None",
-      speed: 4
-    });
-    console.log(`${mockName} Ready`);
-  };
-  createMockLight("Default Mock");
-  if (process.env.MOCKS) {
-    // Set up any extra mock lights if the environment dictates it
-    const mockArray = process.env.MOCKS.split(",");
-    mockArray.forEach(createMockLight);
-  }
-
-  // Start the server
-  console.log("Starting Server");
-  const server = new Server();
-  await server.start(config.serverSettings.port);
+// Start the server
+console.log("Starting Server");
+const server = new Server();
+server.start(config.serverSettings.port).then(() => {
   console.log(
     `🚀 Server ready at http://localhost:${config.serverSettings.port}${
       server.graphqlPath
@@ -77,5 +58,4 @@ const startServer = async () => {
       server.subscriptionsPath
     }`
   );
-};
-startServer();
+});
--- src/lightService/index.js
+++ src/lightService/index.js
@@ -1,3 +1,19 @@
 const createLightService = require("./lightService");
+const mediator = require("../mediator/mediator");
+const dbFactory = require("../lightService/dbFactory");
+const pubsubFactory = require("../lightService/pubsubFactory");
+const config = require("../config");
 
-module.exports = createLightService;
+module.exports = () => {
+  // Create a redis client
+  const redisClient = config.redis.connect(config.redisSettings);
+
+  // Create an MQTT client
+  const mqttClient = config.mqtt.connect(config.mqttSettings);
+
+  const db = dbFactory(redisClient);
+  const pubsub = pubsubFactory({ client: mqttClient, mediator });
+
+  // Start the lightService
+  return createLightService({ mediator, db, pubsub });
+};
